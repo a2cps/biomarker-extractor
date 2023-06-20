4d636c1a14350ad6eee30689b9719cfa60a0050f
Patrick Sadil <psadil@gmail.com>
2023-06-20 18:56:44 -0400

    extract fslanat

--- src/biomarkers/biomarkers.py
+++ src/biomarkers/biomarkers.py
@@ -12,7 +12,6 @@ import prefect_dask
 from dask import config
 
 
-from biomarkers.flows.fslanat import fslanat_flow
 from biomarkers.flows.cat import cat_flow
 from biomarkers.flows.connectivity import connectivity_flow
 from biomarkers.flows.cuff import cuff_flow
@@ -23,24 +22,12 @@ from biomarkers.flows import cuff33
 
 # @prefect.flow(task_runner=SequentialTaskRunner())
 def _main(
-    anats: frozenset[Path] | None = None,
     output_dir: Path = Path("out"),
     cat_dir: Path | None = None,
     rest_subdirs: frozenset[Path] | None = None,
     cuff_subdirs: frozenset[Path] | None = None,
     fmriprep_subdir: frozenset[Path] | None = None,
 ) -> None:
-    if anats:
-        fslanat_flow.with_options(
-            task_runner=prefect_dask.DaskTaskRunner(
-                cluster_kwargs={"n_workers": 40, "threads_per_worker": 1}
-            )
-        )(images=anats, out=output_dir, return_state=True)
-        # fslanat_flow.with_options(
-        #     task_runner=prefect_dask.DaskTaskRunner(
-        #         cluster_kwargs={"n_workers": 20, "threads_per_worker": 1}
-        #     )
-        # )(images=anats, out=output_dir, return_state=True)
     if cat_dir:
         cat_flow.with_options(
             task_runner=prefect_dask.DaskTaskRunner(
@@ -87,49 +74,64 @@ def _main(
 
 @click.command(context_settings={"ignore_unknown_options": True})
 @click.option(
-    "--bids-dir",
-    type=click.Path(
-        exists=True, file_okay=False, dir_okay=True, resolve_path=True, path_type=Path
-    ),
-)
-@click.option(
     "--output-dir",
     default="out",
     type=click.Path(
-        exists=False, file_okay=False, dir_okay=True, resolve_path=True, path_type=Path
+        exists=False,
+        file_okay=False,
+        dir_okay=True,
+        resolve_path=True,
+        path_type=Path,
     ),
 )
 @click.option(
     "--cat-dir",
     type=click.Path(
-        exists=True, file_okay=False, dir_okay=True, resolve_path=True, path_type=Path
+        exists=True,
+        file_okay=False,
+        dir_okay=True,
+        resolve_path=True,
+        path_type=Path,
     ),
 )
 @click.option(
     "--rest-dir",
     type=click.Path(
-        exists=True, file_okay=False, dir_okay=True, resolve_path=True, path_type=Path
+        exists=True,
+        file_okay=False,
+        dir_okay=True,
+        resolve_path=True,
+        path_type=Path,
     ),
 )
 @click.option(
     "--cuff-dir",
     type=click.Path(
-        exists=True, file_okay=False, dir_okay=True, resolve_path=True, path_type=Path
+        exists=True,
+        file_okay=False,
+        dir_okay=True,
+        resolve_path=True,
+        path_type=Path,
     ),
 )
 @click.option(
     "--fmriprep-dir",
     type=click.Path(
-        exists=True, file_okay=False, dir_okay=True, resolve_path=True, path_type=Path
+        exists=True,
+        file_okay=False,
+        dir_okay=True,
+        resolve_path=True,
+        path_type=Path,
     ),
 )
 @click.option(
     "--tmpdir",
-    type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True),
+    type=click.Path(
+        exists=True, file_okay=False, dir_okay=True, resolve_path=True
+    ),
 )
 @click.option("--sub-limit", type=int, default=None)
 def main(
-    bids_dir: Path | None = None,
     output_dir: Path = Path("out"),
     cat_dir: Path | None = None,
     rest_dir: Path | None = None,
@@ -142,14 +144,18 @@ def main(
     # Though, it's also not clear that there is enough benefit to letting Dask spill memory onto
     # disk, so they are staying off for now
     # the last one would be what to try removing firstish
-    config.set({"distributed.worker.memory.rebalance.measure": "managed_in_memory"})
+    config.set(
+        {"distributed.worker.memory.rebalance.measure": "managed_in_memory"}
+    )
     config.set({"distributed.worker.memory.spill": False})
     config.set({"distributed.worker.memory.target": False})
     config.set({"distributed.worker.memory.pause": False})
     config.set({"distributed.worker.memory.terminate": False})
     config.set({"distributed.comm.timeouts.connect": "90s"})
     config.set({"distributed.comm.timeouts.tcp": "90s"})
-    config.set({"distributed.nanny.pre-spawn-environ.MALLOC_TRIM_THRESHOLD_": 0})
+    config.set(
+        {"distributed.nanny.pre-spawn-environ.MALLOC_TRIM_THRESHOLD_": 0}
+    )
 
     if tmpdir:
         os.environ["TMPDIR"] = tmpdir
@@ -157,24 +163,22 @@ def main(
         output_dir.mkdir()
 
     rest_subdirs = (
-        frozenset(list(rest_dir.glob("sub*"))[0:sub_limit]) if rest_dir else None
+        frozenset(list(rest_dir.glob("sub*"))[0:sub_limit])
+        if rest_dir
+        else None
     )
     cuff_subdirs = (
-        frozenset(list(cuff_dir.glob("sub*"))[0:sub_limit]) if cuff_dir else None
+        frozenset(list(cuff_dir.glob("sub*"))[0:sub_limit])
+        if cuff_dir
+        else None
     )
     fmriprep_subdirs = (
         frozenset(list(fmriprep_dir.glob("sub*"))[0:sub_limit])
         if fmriprep_dir
         else None
     )
-    anats = (
-        frozenset(list(bids_dir.glob("sub*/ses*/anat/*T1w.nii.gz"))[0:sub_limit])
-        if bids_dir
-        else None
-    )
     _main(
         output_dir=output_dir,
-        anats=anats,
         rest_subdirs=rest_subdirs,
         cuff_subdirs=cuff_subdirs,
         cat_dir=cat_dir,
