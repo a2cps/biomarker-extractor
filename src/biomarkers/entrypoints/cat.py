7a4f018abb6d12ad9e82434ad45328516c726862
Patrick Sadil <psadil@gmail.com>
2024-10-01 14:05:20 -0400

    assume new cat12 container

--- src/biomarkers/entrypoints/cat.py
+++ src/biomarkers/entrypoints/cat.py
@@ -2,20 +2,14 @@ import logging
 import shutil
 from pathlib import Path
 
-from biomarkers import utils
+from biomarkers import datasets, utils
 from biomarkers.entrypoints import tapismpi
 
 
 class CATEntrypoint(tapismpi.TapisMPIEntrypoint):
-    batchfile: Path
 
-    def get_args(self, nii: Path) -> list[str]:
-        return [
-            "/usr/local/bin/cat_standalone.sh",
-            str(nii),
-            "-b",
-            str(self.batchfile),
-        ]
+    def get_args(self, batchfile: Path) -> list[str]:
+        return ["/opt/spm/spm12", "batch", str(batchfile)]
 
     def check_outputs(self, output_dir_to_check: Path) -> bool:
         out = True
@@ -40,21 +34,39 @@ class CATEntrypoint(tapismpi.TapisMPIEntrypoint):
                 f"Unexpected number of  *T1w.nii.gz found in {tmpd_in}: {maybe_nii}. Taking first."
             )
         nii = maybe_nii[0]
-        nii_out = tmpd_out / "cat" / nii.name
+        nii_out = tmpd_out / "cat12" / nii.name
 
         logging.info(f"Prepping {nii}")
         nii_out.parent.mkdir()
         shutil.copyfile(nii, nii_out)
 
-        return nii_out
+        batch = (
+            datasets.get_cat_batch()
+            .read_text()
+            .replace("<UNDEFINED>", f"'{str(nii_out)}'")
+        )
+        batch_out = nii_out.parent / "batch.m"
+        batch_out.write_text(batch)
+
+        return batch_out
 
     async def run_flow(self, tmpd_in: Path, tmpd_out: Path) -> None:
 
-        staged_in = await self.prep(tmpd_in, tmpd_out)
+        batchfile = await self.prep(tmpd_in, tmpd_out)
         async with utils.subprocess_manager(
             log=tmpd_out / f"cat12_rank-{self.RANK}.log",
-            args=self.get_args(nii=staged_in),
+            args=self.get_args(batchfile),
         ) as proc:
             await proc.wait()
-            staged_in.unlink()
-            (staged_in.parent / staged_in.name.removesuffix(".gz")).unlink()
+
+            # remove extra copies of input files (expect 1 nii, 1 nii.gz)
+            for nii in batchfile.parent.glob("*nii*"):
+                nii.unlink()
+
+            if proc.returncode and proc.returncode > 0:
+                # remove folder so that archiving detects that there was a failure
+                # and sends logs to failure_dst_dir
+                if batchfile.parent.exists():
+                    shutil.rmtree(batchfile.parent)
+                msg = f"cat12 failed with {proc.returncode=}"
+                raise RuntimeError(msg)
