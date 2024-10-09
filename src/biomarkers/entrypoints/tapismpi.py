4062f4c7b291b42e9da15eebeac2313ce33e771b
Patrick Sadil <psadil@gmail.com>
2024-10-09 10:17:39 -0400

    ensure tarball first written to /tmp

--- src/biomarkers/entrypoints/tapismpi.py
+++ src/biomarkers/entrypoints/tapismpi.py
@@ -3,6 +3,7 @@ import logging
 import os
 import shutil
 import socket
+import tarfile
 import tempfile
 import typing
 import uuid
@@ -29,9 +30,9 @@ def configure_mpi_logger() -> None:
 def iterate_byrank_parallel(
     items: typing.Sequence, RANK: int
 ) -> typing.Generator[Path, None, None]:
-    for rank, src in enumerate(items):
+    for rank, item in enumerate(items):
         if rank == RANK:
-            yield src
+            yield item
 
 
 def iterate_byrank_serial(
@@ -76,42 +77,62 @@ class TapisMPIEntrypoint(pydantic.BaseModel):
         return output_dir_to_check.exists()
 
     def archive(self, src: Path) -> None:
-        # parallel
-        tarballs: dict[int, Path] = {}
-        for dst in iterate_byrank_parallel(self.outs, self.RANK):
+        # parallel (across mpi jobs)
+        with tempfile.TemporaryDirectory() as tmpd_:
+            tmpd = Path(tmpd_)
+            tarballs: dict[int, Path] = {}
             try:
                 if self.check_outputs(src):
-                    logging.info(f"Tar'ing {src}")
-                    tar_stem = f"uuid-{self.job_id}_rank-{self.RANK}"
-                    shutil.make_archive(tar_stem, "tar", root_dir=src)
-                    tarballs[self.RANK] = src / f"{tar_stem}.tar"
+                    logging.info(f"Creating tar of {src} in {tmpd}")
+                    tarballs[self.RANK] = (
+                        tmpd / f"uuid-{self.job_id}_rank-{self.RANK}.tar"
+                    )
+                    with tarfile.open(tarballs[self.RANK], mode="w") as tf:
+                        tf.add(src, arcname=".")
             except Exception as e:
                 logging.error(f"Failed to tar {self.outs[self.RANK]=}: {e}")
 
-        # serial
-        for dst in iterate_byrank_serial(self.outs, self.RANK):
+            # src (underneath /tmp) will automatically be deleted after the final copy but
+            # archiving with tar creates a duplicate of all products, which could
+            # be too much for the filesystem. So here we manually delete things.
+            # But we keep the top-level files, because those will be logs
+            # that might need saving
             try:
-                if tarball := tarballs.get(self.RANK):
-                    logging.info(f"Copying {tarball} -> {dst}")
-                    if not dst.exists():
-                        utils.mkdir_recursive(dst, mode=utils.DIR_PERMISSIONS)
-                    shutil.copyfile(tarball, dst)
-                else:
-                    # in case of failures, it's helpful to keep logs around
-                    log_dst = utils.FAILURE_LOG_DST / dst.stem
-                    logging.warning(
-                        f"Failure detected for {self.outs[self.RANK]=}. Copying logs to {log_dst}"
-                    )
-                    if not log_dst.exists():
-                        utils.mkdir_recursive(
-                            log_dst, mode=utils.DIR_PERMISSIONS
-                        )
-                    for log in src.glob("*log"):
-                        shutil.copyfile(log, log_dst / log.name)
+                logging.info(f"Removing unarchived products {src}")
+                for item in src.glob("*"):
+                    if item.is_dir():
+                        shutil.rmtree(src)
             except Exception as e:
                 logging.error(
-                    f"Failed to archive {self.outs[self.RANK]=}: {e}"
+                    f"Failed to remove unarchived products {src}: {e}"
                 )
+
+            # serial
+            for dst in iterate_byrank_serial(self.outs, self.RANK):
+                try:
+                    if tarball := tarballs.get(self.RANK):
+                        logging.info(f"Copying {tarball} -> {dst}")
+                        if not dst.exists():
+                            utils.mkdir_recursive(
+                                dst, mode=utils.DIR_PERMISSIONS
+                            )
+                        shutil.copyfile(tarball, dst / tarball.name)
+                    else:
+                        # in case of failures, it's helpful to keep logs around
+                        log_dst = utils.FAILURE_LOG_DST / dst.stem
+                        logging.warning(
+                            f"Failure detected for {self.outs[self.RANK]=}. Copying logs to {log_dst}"
+                        )
+                        if not log_dst.exists():
+                            utils.mkdir_recursive(
+                                log_dst, mode=utils.DIR_PERMISSIONS
+                            )
+                        for log in src.glob("*log"):
+                            shutil.copyfile(log, log_dst / log.name)
+                except Exception as e:
+                    logging.error(
+                        f"Failed to archive {self.outs[self.RANK]=}: {e}"
+                    )
 
     async def run(self):
         with tempfile.TemporaryDirectory() as _tmpd_in:
