4b7f6d5be429ce7e24d11c998839948034814b6c
Patrick Sadil <psadil@gmail.com>
2025-06-04 08:04:56 -0400

    fix indexing in reading from fsl

--- src/biomarkers/flows/dwi_biomarker1.py
+++ src/biomarkers/flows/dwi_biomarker1.py
@@ -5,6 +5,7 @@ import networkx as nx
 import numpy as np
 import polars as pl
 import pydantic
+from scipy import sparse
 
 
 class Module(pydantic.BaseModel):
@@ -21,18 +22,20 @@ MODULE_INFO = (
 
 
 def read_matrix1(src: Path) -> pl.DataFrame:
+    # need to symmetrize because probtrackx stores
+    # both source -> target and target -> source,
+    # but only one of each edge
+    i, j, value = np.loadtxt(src).T
+    coo = sparse.coo_array((value, (np.astype(i - 1, int), np.astype(j - 1, int))))
+    coo += coo.T
+    coo /= 2
     return (
-        pl.read_csv(src, has_header=False)
+        pl.DataFrame(coo.todense())
+        .with_row_index("source")
+        .unpivot(index="source", variable_name="target")
         .with_columns(
-            pl.col("column_1")
-            .str.split("  ")
-            .list.to_struct(fields=["source", "target", "value"])
-        )
-        .unnest("column_1")
-        .with_columns(
-            pl.col("source").cast(pl.UInt16),
-            pl.col("target").cast(pl.UInt16),
-            pl.col("value").cast(pl.Float64),
+            pl.col("target").str.strip_prefix("column_").cast(pl.UInt16) + 1,
+            pl.col("source").cast(pl.UInt16) + 1,
         )
         .filter(pl.col("source") < pl.col("target"))
         .with_columns(value=pl.col("value") / pl.col("value").abs().max())
@@ -49,10 +52,7 @@ def read_matrix1_coordinates(src: Path) -> pl.DataFrame:
         )
         .unnest("column_1")
         .drop("x", "y", "z")
-        .with_columns(
-            pl.col("roi").cast(pl.UInt8),
-            pl.col("index").cast(pl.UInt16),
-        )
+        .with_columns(pl.col("roi").cast(pl.UInt8), pl.col("index").cast(pl.UInt16))
     )
 
 
@@ -60,25 +60,28 @@ def threshold_proportional_bin(
     d: pl.DataFrame, target_density: float = 0.1
 ) -> pl.DataFrame:
     density: float = (d["value"] > 0).mean()  # type: ignore
-    if density < target_density:
+    if density <= target_density:
         return d.with_columns(pl.col("value").cast(pl.Boolean))
 
     return d.with_columns(
-        value=pl.when(pl.col("value") < pl.col("value").quantile(1 - target_density))
-        .then(False)
-        .otherwise(True)
+        value=pl.col("value") > pl.col("value").quantile(1 - target_density)
     )
 
 
 def read_weighted_matrix1_df(src: Path, target_density: float = 0.1) -> pl.DataFrame:
-    matrix1 = read_matrix1(src / "fdt_matrix1.dot.gz")
     coordinates = read_matrix1_coordinates(src / "coords_for_fdt_matrix1")
+    matrix1 = read_matrix1(src / "fdt_matrix1.dot.gz")
     return (
-        coordinates.rename({"index": "source", "roi": "roi_source"})
-        .join(coordinates.rename({"index": "target", "roi": "roi_target"}), how="cross")
-        .filter(pl.col("source") < pl.col("target"))
-        .join(matrix1, on=["source", "target"], how="left")
-        .fill_null(strategy="zero")
+        matrix1.join(
+            coordinates.rename({"roi": "roi_source"}),
+            left_on="source",
+            right_on="index",
+        )
+        .join(
+            coordinates.rename({"roi": "roi_target"}),
+            left_on="target",
+            right_on="index",
+        )
         .pipe(threshold_proportional_bin, target_density)
         .with_columns(pl.col("source") - 1, pl.col("target") - 1)
     )
