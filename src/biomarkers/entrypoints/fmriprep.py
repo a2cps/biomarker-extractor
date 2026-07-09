1a6a0f8c05e52a8ce8304528b181075906b2cfd9
Patrick Sadil <psadil@gmail.com>
2026-07-09 19:19:30 -0400

    reduce fmriprep outputs

--- src/biomarkers/entrypoints/fmriprep.py
+++ src/biomarkers/entrypoints/fmriprep.py
@@ -53,6 +53,8 @@ class FMRIPRepEntrypoint(tapismpi.TapisMPIEntrypoint):
             "/app/.pixi/envs/fmriprep/bin/fmriprep",
             "--notrack",
             "--return-all-components",
+            "--ignore fmap-jacobian slicetiming",
+            "--level resampling",
         ]
         if self.anat_only and self.anat_only[self.RANK]:
             args.append("--anat-only")
